f72a753927d7c412cb1d8869f6af9312cf2ccc74
jh <parkje09270@gmail.com>
2021-12-31 23:12:12 +0900

    [ADD] 에라토스테네스의 체

--- algorithm/chap01.py
+++ algorithm/chap01.py
@@ -9,3 +9,19 @@ def is_prime_number(x):
 
 print(is_prime_number(4))
 print(is_prime_number(7))
+
+# 에라토스테네스의 체
+# 2 ~ 1000 까지늬 모든 수에 대하여 소수 판별
+n = 1000
+array = [True for i in range(n + 1)]
+
+for i in range(2, int(math.sqrt(n)) + 1):
+    if array[i] == True:
+        j = 2
+        while i * j <= n:
+            array[i * j] = False
+            j += 1
+
+for i in range(2, n + 1):
+    if array[i]:
+        print(i, end=' ')
